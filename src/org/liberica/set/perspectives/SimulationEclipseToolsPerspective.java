90feaa31aae4d850e4676ca8860b9f688bd222b2
zaotroy <zaotroy@gmail.com>
2012-10-11 13:18:44 +0300

    Add perspective to plug-in.

---
+++ src/org/liberica/set/perspectives/SimulationEclipseToolsPerspective.java
@@ -0,0 +1,24 @@
+package org.liberica.set.perspectives;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class SimulationEclipseToolsPerspective implements IPerspectiveFactory {
+
+	private IPageLayout factory;
+
+	public SimulationEclipseToolsPerspective() {
+		super();
+	}
+
+	public void createInitialLayout(IPageLayout factory) {
+		this.factory = factory;
+		addViews();
+	}
+
+	private void addViews() {
+		IFolderLayout bottom = factory.createFolder("bottomRight", IPageLayout.BOTTOM, 0.75f, factory.getEditorArea());
+		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+	}
+}

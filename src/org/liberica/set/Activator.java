e80e29f06f3f09cf74b94e76e1dd983a5f790882
zaotroy <zaotroy@gmail.com>
2012-10-13 22:09:13 +0200

    Remove eclipse UI.

--- src/org/liberica/set/Activator.java
+++ src/org/liberica/set/Activator.java
@@ -1,28 +1,28 @@
 package org.liberica.set;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
+//import org.eclipse.ui.plugin.AbstractUIPlugin;
+//import org.osgi.framework.BundleContext;
 
-public class Activator extends AbstractUIPlugin {
+public class Activator /*extends AbstractUIPlugin*/ {
 
-	public static final String PLUGIN_ID = "org.liberica.set";
-
-	private static Activator plugin;
-
-	public Activator() {
-	}
-
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	public static Activator getDefault() {
-		return plugin;
-	}
+//	public static final String PLUGIN_ID = "org.liberica.set";
+//
+//	private static Activator plugin;
+//
+//	public Activator() {
+//	}
+//
+//	public void start(BundleContext context) throws Exception {
+//		super.start(context);
+//		plugin = this;
+//	}
+//
+//	public void stop(BundleContext context) throws Exception {
+//		plugin = null;
+//		super.stop(context);
+//	}
+//
+//	public static Activator getDefault() {
+//		return plugin;
+//	}
 }
